4fbd1d13b81704720ac14127282f089ab430f9ba
Lubajkin Vladislav <lubajkinvlad@gmail.com>
2025-07-05 00:00:22 +0300

    туду эдитор на хуки переписал

--- src/components/ToDoEditor.jsx
+++ src/components/ToDoEditor.jsx
@@ -1,45 +1,84 @@
-import {Component} from "react";
+import {useState} from "react";
 
-class ToDoEditor extends Component {
-    state={
-        message: '',
-    }
 
-    handleChange = (e) => {
-        this.setState({
-            message: e.target.value,
+function ToDoEditor({onAddTodo, onToggle}) {
+    const [message, setMessage] = useState('')
 
-        });
-    }
 
-    handleSubmit = (e) => {
-        const {onAddTodo, onToggle} = this.props
+    const handleSubmit = (e) => {
+
         e.preventDefault();
-        if(this.state.message === ''){
+        if (message === '') {
             alert('Введи че нить')
             return
         }
-       onAddTodo(this.state.message);
-        this.setState({message: ''});
+        onAddTodo(message);
+        setMessage('');
         onToggle()
-
-
     }
 
-    render() {
-        return(
-            <form className='flex flex-col max-w-2xl ' onSubmit={this.handleSubmit}>
+    return (
+        <form className='flex flex-col max-w-2xl ' onSubmit={handleSubmit}>
                 <textarea
                     className='textarea p-2 resize-none'
-                    value={this.state.message}
-                    onChange={this.handleChange}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                     placeholder='пиши сюдой...'>
 
                 </textarea>
-                <button className='btn-base w-full !rounded-md' type='submit'>Добавить</button>
-            </form>
-        );
-    }
+            <button className='btn-base w-full !rounded-md' type='submit'>Добавить</button>
+        </form>
+    );
+
+
 }
+
+
+
+
+
+// import {Component} from "react";
+//
+// class ToDoEditor extends Component {
+//     state={
+//         message: '',
+//     }
+//
+//     handleChange = (e) => {
+//         this.setState({
+//             message: e.target.value,
+//
+//         });
+//     }
+//
+//     handleSubmit = (e) => {
+//         const {onAddTodo, onToggle} = this.props
+//         e.preventDefault();
+//         if(this.state.message === ''){
+//             alert('Введи че нить')
+//             return
+//         }
+//        onAddTodo(this.state.message);
+//         this.setState({message: ''});
+//         onToggle()
+//
+//
+//     }
+//
+//     render() {
+//         return(
+//             <form className='flex flex-col max-w-2xl ' onSubmit={this.handleSubmit}>
+//                 <textarea
+//                     className='textarea p-2 resize-none'
+//                     value={this.state.message}
+//                     onChange={this.handleChange}
+//                     placeholder='пиши сюдой...'>
+//
+//                 </textarea>
+//                 <button className='btn-base w-full !rounded-md' type='submit'>Добавить</button>
+//             </form>
+//         );
+//     }
+// }
 
 export default ToDoEditor
